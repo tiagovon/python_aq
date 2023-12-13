2a4114a3cd966236c6ba4da3b07b9ac1e62ea99f
tiagovon <tiago3242@gmail.com>
2023-12-13 15:17:58 -0300

    ola mundo

--- hello.py
+++ hello.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
-print("hello world!")
-print(432-312)
+
+import os
+
+lang = os.getenv("LANG")
+
+current_language = lang[:5]
+
+msg = "hello world!"
+
+if current_language =="pt_BR":
+    msg = "Ola mundo!"
+elif current_language =="it_IT":
+    msg = "Ciao,Mondo!" 
+
+print(msg)
+
 

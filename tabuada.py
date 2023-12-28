8a813728fc05802d877ec11391bf23ea36db7d5a
tiagovon <tiago3242@gmail.com>
2023-12-28 15:27:43 -0300

    tabuada

---
+++ tabuada.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env
+
+
+
+   
+base = list(range(1,11))
+
+
+for numero in base:
+    print(f"tabuada do {numero}")
+    numero_base = numero 
+    for _ in base:
+        print(numero * _) 
+    
+

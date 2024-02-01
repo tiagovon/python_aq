b65c81ad38dc9e36f4d72eadfb509caafb603b58
tiagovon <tiago3242@gmail.com>
2024-02-01 15:45:08 -0300

    super hello

--- hello.py
+++ hello.py
@@ -1,19 +1,37 @@
 #!/usr/bin/env python3
 
 import os
+import sys
 
-lang = os.getenv("LANG","en_US")
+argumentos ={
+   "lang":None,
+   "count":1,
+}
+for args in sys.argv[1:]:
+    key, value = args.split("=")
+    key = key.lstrip("-").strip()
+    if key not in argumentos:
+         print(f"ivalid option '{key}'")
+         sys.exit()
+    argumentos[key]= value
 
-current_language = lang[:5]
 
-msg = "hello world!"
+current_language = argumentos["lang"]
+if current_language is None:
+    if "LANG" in os.environ:
+         current_language = os.getenv("LANG","en_US")
+    else:
+        current_language = input("choose a language")
 
-if current_language =="pt_BR":
-    msg = "Ola mundo!"
-elif current_language =="it_IT":
-    msg = "Ciao,Mondo!" 
+current_language = current_language[:5]
 
-print(msg)
+
+msg = {
+"en_US":"Hello World",
+"pt_BR":"Ola mundo",
+"it_IT":"Ciao Mondo"
+}
+print(msg[current_language] * int(argumentos["count"]))
 
 
 

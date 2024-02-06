433ea084624a4088310c9f1734f3c5954121e51d
tiagovon <tiago3242@gmail.com>
2024-02-06 12:00:27 -0300

    1.0

--- gmail.py
+++ gmail.py
@@ -7,6 +7,25 @@ if not argumentos:
     sys.exit(1)
 
 filename = argumentos[0]
+email_file_name = argumentos[1]
+
 
 path = os.curdir
-file = os.path.join(path,"")
+file = os.path.join(path,filename)
+email_file = os.path.join(path,email_file_name)
+
+
+for line in open(file):
+    name , email = line.split(",")
+    print(f"eviando email para {email}")
+    print(
+        open(email_file).read()
+       %{
+       "nome":name,
+       "produto":"caneta",
+       "texto":"escrever muito bem",
+       "link":"http//canetaslegais.com",
+       "quantidade":1,
+       "preco":50.5,       }
+    )
+    print("-" *50)

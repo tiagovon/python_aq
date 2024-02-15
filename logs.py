2589d5a334e224a0aaf85b91efacec8f2645b7cc
tiagovon <tiago3242@gmail.com>
2024-02-15 14:25:48 -0300

    log file

--- logs.py
+++ logs.py
@@ -2,15 +2,25 @@
 
 import logging
 import os
+from logging import handlers
 #TODO: usar funçao
 #TODO: usar lib
 log_level= os.getenv("LOG_LEVEL","WARNING").upper()
 log = logging.Logger("logs.py",log_level)
-ch= logging.StreamHandler()
-ch.setLevel(log_level)
+#ch= logging.StreamHandler()
+#ch.setLevel(log_level)
+fh = handlers.RotatingFileHandler(
+   "meulog.log", 
+   maxBytes=300, # 10**6
+   backupCount=10,)
+fh.setLevel(log_level)
+
+
 fnt = logging.Formatter( '%(asctime)s %(name)s %(levelname)s l:%(lineno)d f:%(filename)s: %(message)s')
-ch.setFormatter(fnt)
-log.addHandler(ch)
+#ch.setFormatter(fnt)
+fh.setFormatter(fnt)
+#log.addHandler(ch)
+log.addHandler(fh)
 
 """
 log.debug("Mensagem pro dev , qe , sysadmin")
@@ -20,6 +30,10 @@ log.error("erro que afeta uma unica exuxao")
 log.critical("erro geral ex :banco de dados sumiu")
 """
 
-    
+try:
+    1/0
+
+except ZeroDivisionError as e:
+    log.error("1/0")
 
     

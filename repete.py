40b06a0542d266bac516d964310a20488b964ebd
tiagovon <tiago3242@gmail.com>
2024-02-21 12:00:44 -0300

    for and while

---
+++ repete.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+#for
+#numbers = range(1,11)
+
+#for number in numbers:
+    #par = number % 2 ==0
+   #if par:
+       # print(number)
+
+   #else:
+       #continue
+   # print("mais e mais")
+
+#nova = [1,2,3]
+
+# list = [n for n in list ]
+#dobrada = [n * 2 for n in nova if n > 1]
+#print(dobrada)
+
+
+
+# dict = {key:value for n in dict }
+#dados = {line.split(":")[0]: line.split(":")[1].strip()for line in open("post.txt") if ":"in line }
+#print(dados)
+
+
+#while
+
+
+end_loop = True
+while end_loop:
+        resposta = input("quer continar? s ou n")
+        if resposta == "n":
+            end_loop = False
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

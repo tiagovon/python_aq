15f5afef3f3d173ae0273f2545444cd2f96574b6
tiagovon <tiago3242@gmail.com>
2024-02-26 14:55:26 -0300

    verson linxtips

--- exercicos.py
+++ exercicos.py
@@ -144,86 +144,143 @@ reservado.
 
 #ex4
 
+# import os
+# import sys
+
+
+# path = os.curdir
+# filepath =os.path.join(path,"quartos.txt")
+# arquivopath = os.path.join(path,"reservas.txt")
+# numeros_de_quartos= []
+# loop = True
+# sub_loop = True
+# while loop:
+#    dic_dados = {}
+#    dic_num = {}
+#    
+#    for line in  open(filepath):
+#        numero , quarto , preço  = line.split(",")
+#        print(f"temos o/a {quarto} de numero: {numero} "  
+#         f"com o preço de {preço}")
+#       dic_dados[quarto] = int(preço)
+#        dic_num[numero] = {quarto:int(preço)}
+    
+    
+#    nome_usr = input("seu nome ")
+#    numero_user = input("qual e o numero do quarto que "
+#    "voce quer resevar? ")
+#    numeros_de_dias = input("quantos dias voce quer passar? ")
+#    while sub_loop:
+#         if numero_user in numeros_de_quartos:
+#                 print("esse quarto ja esta resevado")
+#                 input(f"tente outro numero ")
+#         else:
+#            sub_loop = False       
+    
+#    numeros_de_quartos.append(numero_user)
+    
+    
+    
+#    for keys in dic_num[numero_user]:
+#        preço_user = dic_dados[keys]
+    
+#    valor = preço_user * int(numeros_de_dias)
+    
+#    print(f"o valor vai ficar {valor}")
+    
+#    with open(arquivopath,"a") as arquivo:
+#        arquivo.write(nome_usr + ",")
+#        arquivo.write(numero_user + ",")
+#        arquivo.write(numeros_de_dias + "\n")
+
+#    final_loop = input("quer conitunar? y/n: ")
+#    if final_loop  == "n":
+#        loop = False
+    #   reservas.txt
+
+#with open(arquivopath , "w") as arquivo:
+#   pass
+
+
+
+# ex 4  com o linutipis 
+
 import os
 import sys
-
+import logging
 
 path = os.curdir
 filepath =os.path.join(path,"quartos.txt")
 arquivopath = os.path.join(path,"reservas.txt")
 
 dic_dados = {}
-dic_num = {}
-
-for line in  open(filepath):
-    numero , quarto , preço  = line.split(",")
-    print(f"temos o/a {quarto} de numero: {numero} "  
-     f"com o preço de {preço}")
-    dic_dados[quarto] = int(preço)
-    dic_num[numero] = {quarto:int(preço)}
-
-
-nome_usr = input("seu nome ")
-numero_user = input("qual e o numero do quarto que "
-"voce quer resevar? ")
-
-numeros_de_quartos = []
-for line in open(arquivopath):
-        nome ,  numero  , dias = line.split(",")
-        numeros_de_quartos.append(int(numero))
-if int(numero_user) in numeros_de_quartos:
-        print("esse quarto ja esta resevado")
-        sys.exit(1)   
-
-numeros_de_dias = input("quantos dias voce quer passar? ")
-
-
-for keys in dic_num[numero_user]:
-    preço_user = dic_dados[keys]
-
-valor = preço_user * int(numeros_de_dias)
-
-print(f"o valor vai ficar {valor}")
-
-with open(arquivopath,"a") as arquivo:
-    arquivo.write(nome_usr + ",")
-    arquivo.write(numero_user + ",")
-    arquivo.write(numeros_de_dias + "\n")
-#   reservas.txt
-
-#cliente, quarto, dias
+dic_reserva =  {} 
 
 
 
+try:
+         for line in  open(arquivopath):
+             nome , num_quarto_re , dias  = line.split(",")
+             dic_reserva[int(num_quarto_re)] = {
+                 "nome":nome,
+                 "dias":dias            
+                      }
+except FileNotFoundError:
+    logging.error("arquivo na existe")
+    sys.exit(1)   
+
+try:
+    for line in  open(filepath):
+        numero , quarto , preço  = line.split(",")
+        dic_dados[int(numero)] = {
+            "nome":quarto,
+            "preço":float(preço),
+            "disposnivel": False if  int(numero) in dic_reserva else True         
+        }
+except FileNotFoundError:
+    logging.error("arquivo na existe")
+    sys.exit(1)
+
+if len(dic_reserva) == len(dic_dados):
+    print("hotel cheio")
+    sys.exit(1)
+
+nome = input("Nome do Cliente ").strip()  
+print("-"*40)
+print("Escolha um quarto")
+print("-"*40)
+
+for codigo, dados in dic_dados.items():
+    disposnivel =  "ocupado" if not dados["disposnivel"] else "livre"
+    print(f"{codigo}-{dados['nome']}-R${dados['preço']}"
+  f"-{disposnivel}")
+
+
+try:
+   num_quarto = int(input("numero do quarto ").strip())
+   if not dic_dados[num_quarto]["disposnivel"]:
+        print(f"o quarto {num_quarto} ja esta ocupado")
+        sys.exit(1)
+except ValueError:
+    logging.error("numero inavalido")
+    sys.exit(1)
+except KeyError:
+    print(f"o quarto {num_quarto} nao existe ")
+    sys.exit(1)
+
+try:
+   dias = int(input("dias que vai ficar ").strip())
+except ValueError:
+    logging.error("numero inavalido")
+    sys.exit(1)
+
+total = dic_dados[num_quarto]["preço"] * dias
+print(f"vai custar {total}")
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+with open(arquivopath,"a") as file:
+    file.write(f"{nome},{num_quarto},{dias}\n")
 
 
 
